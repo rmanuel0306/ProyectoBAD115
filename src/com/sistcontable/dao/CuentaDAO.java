4a952c287ca39c51d2509cbcff075e5553998daa
Administrador <Administrador@winserver.oracle>
2015-06-21 14:42:23 -0600

    cuenta DAO modificada

--- src/com/sistcontable/dao/CuentaDAO.java
+++ src/com/sistcontable/dao/CuentaDAO.java
@@ -9,6 +9,7 @@ import com.sistcontable.hibernate.util.HibernateUtil;
 import com.sistcontable.model.Cuenta;
 import java.util.ArrayList;
 import java.util.List;
+import org.hibernate.Query;
 import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 
@@ -47,6 +48,36 @@ public class CuentaDAO {
         }
     }
     
+    public Cuenta obtenerCuentaByID(String id){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        List<Cuenta> lista =new ArrayList();
+        try{
+            String query = "select * from cuenta where ID_CUENTA=:idCuenta";
+            SQLQuery sqlQuery = session.createSQLQuery(query);
+            sqlQuery.addEntity(Cuenta.class);
+            sqlQuery.setParameter("idCuenta", id); 
+            List result = sqlQuery.list();
+            
+            for(int i=0;i<result.size();i++){
+                System.out.println("id: ");
+                Cuenta cuenta = (Cuenta)result.get(i);
+                
+                System.out.println("idCuenta: "+cuenta.getIdCuenta());
+                System.out.println("estado: "+cuenta.getEstado().getNombreEstado());
+                System.out.println("rubro: "+cuenta.getRubro().getNombreRubro());
+                System.out.println("nombreCuenta: "+cuenta.getNombreCuenta());
+                System.out.println("cuentaMayor: "+cuenta.getCuentaMayor());
+            }    
+            return (Cuenta)result.get(0);
+        }catch (Exception e){
+            System.out.println("ERROR " + e);
+            lista = null;
+            return null;
+        }finally{
+            session.close();
+        }
+    }
     
         public List<Cuenta> searchAllCuentas(){
         Session session = HibernateUtil.getSessionFactory().getCurrentSession();
@@ -58,15 +89,15 @@ public class CuentaDAO {
             sqlQuery.addEntity(Cuenta.class);               
             lista = sqlQuery.list(); 
             
-            for(int i=0; i<lista.size() ; i++){
+             for(int i=0; i< lista.size(); i++){
                 System.out.println("-------------------------------------------------");
                 System.out.println("idCuenta: "+lista.get(i).getIdCuenta());
                 System.out.println("estado: "+lista.get(i).getEstado().getNombreEstado());
                 System.out.println("rubro: "+lista.get(i).getRubro().getNombreRubro());
                 System.out.println("nombreCuenta: "+lista.get(i).getNombreCuenta());
                 System.out.println("cuentaMayor: "+lista.get(i).getCuentaMayor());
-                
-            }
+             }  
+            
                 
             
         }catch (Exception e){
@@ -76,7 +107,31 @@ public class CuentaDAO {
             session.close();
         }
         return lista;
-    } 
+    }
+        
+        public boolean saveCuenta(Cuenta cuenta){
+        Session session = HibernateUtil.getSessionFactory().getCurrentSession();
+        session.beginTransaction();
+        Boolean isInsert = Boolean.FALSE;        
+        try{          
+            String query = "insert into Cuenta(ID_CUENTA, ID_ESTADO, ID_RUBRO, NOMBRE_CUENTA, CUENTA_MAYOR) values(:idCuenta, :estado, :rubro, ':nombreCuenta', :cuentaMayor)";
+            Query sqlQuery = session.createSQLQuery(query).addEntity(Cuenta.class)
+                    .setParameter("idCuenta", cuenta.getIdCuenta())
+                    .setParameter("estado", cuenta.getEstado())
+                    .setParameter("rubro", cuenta.getRubro())
+                    .setParameter("nombreCuenta", cuenta.getNombreCuenta())
+                    .setParameter("cuentaMayor", cuenta.getCuentaMayor());
+            sqlQuery.executeUpdate();
+            System.out.println(sqlQuery.toString());
+            isInsert = Boolean.TRUE;        
+        }catch (Exception e){
+            System.out.println("ERROR " + e);
+            //lista = null;
+        }finally{
+            session.close();
+        }
+        return isInsert;
+    }    
     
     
 }
